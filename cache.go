2620be68020c414bef86015e9a2bdf7c722d6b5a
JonathanRizocki <j.rizocki@gmail.com>
2024-04-17 18:07:41 -0700

    Initial commit of generic cache

---
+++ cache.go
@@ -0,0 +1,32 @@
+package cache
+
+// Cache is a key-value storage.
+type Cache[K comparable, V any] struct {
+	data map[K]V
+}
+
+// New creates a usable cache
+func New[K comparable, V any]() Cache[K, V] {
+	return Cache[K, V]{
+		data: make(map[K]V),
+	}
+}
+
+// Read returns the associated value for a key,
+// and returns a boolean to true if the key is present
+func (c *Cache[K, V]) Read(key K) (V, bool) {
+	val, found := c.data[key]
+	return val, found
+}
+
+func (c *Cache[K, V]) Upsert(key K, val V) error {
+	c.data[key] = val
+
+	// Do not return an error yet.
+	// It can happen in the future.
+	return nil
+}
+
+func (c *Cache[K, V]) Delete(key K) {
+	delete(c.data, key)
+}
---
+++ cache_internal_test.go
@@ -0,0 +1,45 @@
+package cache_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	cache "learngo-pockets/genericcache"
+)
+
+func TestCache(t *testing.T) {
+	c := cache.New[int, string]()
+
+	c.Upsert(5, "fünf")
+
+	v, found := c.Read(5)
+	assert.True(t, found)
+	assert.Equal(t, "fünf", v)
+
+	c.Upsert(5, "pum")
+
+	v, found = c.Read(5)
+	assert.True(t, found)
+	assert.Equal(t, "pum", v)
+
+	c.Upsert(3, "drei")
+
+	v, found = c.Read(3)
+	assert.True(t, found)
+	assert.Equal(t, "drei", v)
+
+	v, found = c.Read(5)
+	assert.True(t, found)
+	assert.Equal(t, "pum", v)
+
+	c.Delete(5)
+
+	v, found = c.Read(5)
+	assert.False(t, found)
+	assert.Equal(t, "", v)
+
+	v, found = c.Read(3)
+	assert.True(t, found)
+	assert.Equal(t, "drei", v)
+}
